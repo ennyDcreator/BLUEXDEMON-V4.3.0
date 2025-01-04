1c2bb5678ac091bedc5d243977c4c102f0489ab1
ennyDcreator <157379310+ennyDcreator@users.noreply.github.com>
2025-01-04 10:27:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2347048521468"
 
-global.botNumber = ["2349019529423"]
+global.botNumber = ["2347048521468"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
@@ -21,8 +21,8 @@ global.antiSpam = true
 global.sign = "☃️❄️"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
-global.bankname = "BLUE"
-global.accnumber = "7041039367"
+global.bankname = "ONIYIDE MICHAEL ENIOLA"
+global.accnumber = "7048521468"
 global.language = "en"
 global.sessionName = "session"
 global.pairingCode = true 
